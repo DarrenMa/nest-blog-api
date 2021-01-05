3d159e0bd3d4fc4eeda77745b531ecf22fa3d2bf
Darren Ma <darrenma1986@gmail.com>
2021-01-05 13:35:57 +0100

    finish database setup

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { databaseProviders } from './database.providers';
 
-@Module({})
+@Module({
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
+})
+
 export class DatabaseModule {}
